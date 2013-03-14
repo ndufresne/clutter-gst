27a1183be650e07bfe30c56288792f7409015792
Lionel Landwerlin <llandwerlin@gmail.com>
2013-03-14 17:18:25 +0000

    playback: add missing header

--- clutter-gst/clutter-gst-playback.h
+++ clutter-gst/clutter-gst-playback.h
@@ -1,4 +1,35 @@
-/* clutter-gst-playback.h */
+/*
+ * Clutter-GStreamer.
+ *
+ * GStreamer integration library for Clutter.
+ *
+ * clutter-gst-player.h - Wrap some convenience functions around playbin2
+ *
+ * Authored By Damien Lespiau    <damien.lespiau@intel.com>
+ *             Lionel Landwerlin <lionel.g.landwerlin@linux.intel.com>
+ *             Matthew Allum     <mallum@openedhand.com>
+ *             Emmanuele Bassi   <ebassi@linux.intel.com>
+ *             Andre Moreira Magalhaes <andre.magalhaes@collabora.co.uk>
+ *
+ * Copyright (C) 2006 OpenedHand
+ * Copyright (C) 2009-2013 Intel Corporation
+ * Copyright (C) 2012 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #if !defined(__CLUTTER_GST_H_INSIDE__) && !defined(CLUTTER_GST_COMPILATION)
 #error "Only <clutter-gst/clutter-gst.h> can be include directly."
